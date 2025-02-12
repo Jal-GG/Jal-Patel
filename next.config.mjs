5fd378ff39a47bf6853c964f3369e22281488dde
Jal-GG <jalvrund2017@gmail.com>
2025-02-12 21:02:37 +0530

    github pages setup

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  output: 'export',
+  images: {
+    unoptimized: true, // Required if you're using Next.js Image component
+  },
+  trailingSlash: true, // Ensures proper routing on GitHub Pages
 }
  
 const withMDX = createMDX({
